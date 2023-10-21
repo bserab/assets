7d2a36ac4e5796eb65fe843eefc052a7a960cc6f
bserab <chmt22074@g.nihon-u.ac.jp>
2023-10-21 18:10:43 +0900

    compo.htmls

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import Main from "./components/maintxt";
+import Footer from "./components/footer";
+
+export default function App() {
+    return(
+        <div>
+        <Header />
+        <Main />
+        <Footer />
+        </div>
+    );
+  }
---
+++ src/components/maintxt.jsx
@@ -0,0 +1,12 @@
+import Aside from "./aside";
+import Text from "./text";
+
+export default function maintxt(){
+    return(
+            <main>
+                <Text />
+                <Aside />
+            </main>
+        
+    );
+}
